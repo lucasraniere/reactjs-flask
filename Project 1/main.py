dfff7b120ecabe4a9a8e45f0fcedc5524bb392a7
Lucas Raniére <lucas.raniere94@gmail.com>
2021-08-30 16:35:03 -0300

    deleting project 1

--- Project 1/main.py
+++
@@ -1,9 +0,0 @@
-import flask
-
-app = flask.Flask('__main__')
-
-@app.rout('/')
-def my_index():
-    return flask.render_template('index.html', token='Hello Flask+React')
-
-app.run(debug=True)
--- README.md
+++
@@ -1,2 +0,0 @@
-# reactjs-flask
-Learning backend with Flask
